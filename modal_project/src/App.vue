93e9e9c2fdd1cdc15c42dfba2aeaaf832aa3739e
Ronit Wilson <ronit.study@gmail.com>
2023-05-01 08:49:10 +0000

    initial changes

---
+++ modal_project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>  First vue app by {{ user }}</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      user: 'Ronit'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
